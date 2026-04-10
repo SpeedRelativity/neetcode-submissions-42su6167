86a55b4a7bd56685f3c17d07e3f0171c1ee3b983
Nechar KC <126724860+SpeedRelativity@users.noreply.github.com>
2026-04-10 15:22:54 -0700

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+
+        int length;
+        char delimeter = '#';
+        
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < strs.size(); i++){
+            ans.append(strs.get(i).length()).append(delimeter).append(strs.get(i));
+        }
+
+        return ans.toString();
+        
+
+    }
+
+    // 5#Hello6#World!
+
+    public List<String> decode(String str) {
+        
+        List<String> output = new ArrayList<>();
+        int i = 0; int j = 0;
+
+        while(i < str.length()){
+            j = i;
+            while(str.charAt(j) != '#'){
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i,j));
+            
+            i = j + 1;
+            output.add(str.substring(i,i + length));
+            i = i + length;
+        }
+
+        return output;
+
+    }
+}
