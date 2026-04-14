6457cc348190ef2be97653fe95372fcfe1c434e9
Nechar KC <126724860+SpeedRelativity@users.noreply.github.com>
2026-04-13 23:29:34 -0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int left = 0;
+        int right = matrix.length - 1;
+
+        int matrixIndex = -1;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            int last = matrix[left].length - 1;
+            if(matrix[mid][last] < target){
+                left++;
+            }
+            else if(matrix[mid][0] > target){
+                right--;
+            }
+            else{
+                matrixIndex = mid;
+                break;
+            }
+        }
+
+        if(matrixIndex == -1) return false;
+
+        left = 0;
+        right = matrix[0].length - 1;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(matrix[matrixIndex][mid] == target){
+                return true;
+            }
+            else if(matrix[matrixIndex][mid] > target){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+
+
+        return false;
+
+
+
+    }
+}
