ea7f108a63214ae1b12b65b23665102daef972f9
Nechar KC <126724860+SpeedRelativity@users.noreply.github.com>
2026-04-17 14:02:36 -0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.java
@@ -0,0 +1,38 @@
+class TimeMap {
+
+    
+    HashMap<String, TreeMap<Integer, String>> timeMap;
+    public TimeMap() {
+        timeMap = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+
+        if(!timeMap.containsKey(key)){
+            timeMap.put(key, new TreeMap<>());
+        }
+        timeMap.get(key).put(timestamp, value);
+        
+    }
+    
+    public String get(String key, int timestamp) {
+        
+        if(!timeMap.containsKey(key)){
+            return "";
+        }
+        
+        if(timeMap.get(key).containsKey(timestamp)){
+            return timeMap.get(key).get(timestamp);
+        }
+        else{
+            Map.Entry<Integer,String> entry = timeMap.get(key).floorEntry(timestamp);
+            if (entry == null){
+                return "";
+            }
+            else{
+                return entry.getValue();
+            }
+        }
+        
+    }
+}
